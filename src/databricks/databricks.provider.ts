e1ef0a5558b3b612dfea1fe6c9c4dfc86fab812f
Marcos Vitor Dias dos Santos <marcos.vitor@presidencia.gov.br>
2025-02-11 09:01:43 -0300

    Feat: Dotenv install

--- src/databricks/databricks.provider.ts
+++ src/databricks/databricks.provider.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DBSQLClient } from '@databricks/sql';
+require('dotenv').config()
+
 
 @Injectable()
 export class DatabricksConnectionProvider {
@@ -9,9 +11,9 @@ export class DatabricksConnectionProvider {
   private client = new DBSQLClient();
 
   async connect() {
-    const serverHostname = 'adb-780571104098995.15.azuredatabricks.net'
-    const httpPath = '/sql/1.0/warehouses/2da57dd8e33c5731'
-    const token = 'dapi77e18782891d10dcdcb7d595e5117771'
+    const serverHostname = `${process.env.SERVERHOSTNAME}`
+    const httpPath = `${process.env.HTTPPATH}`
+    const token = `${process.env.TOKEN}`
 
     await this.client.connect({ host: serverHostname, path: httpPath, token });
     console.log('Connected to Databricks.');
